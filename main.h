cec80dfa478d9f6a6665c0d05c2abd0efcd6173b
Ashwini Bhandari <141109400+bhandariashwini@users.noreply.github.com>
2025-07-27 14:41:30 +0530

    main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+
+//main.h
+#include "types.h"
+void CfgInterrupt(void);
+void FIQ_Interrupt(void) __irq;
+int check_schedule_time(void);
+void EditRTC(void);
+void EScheduleTime(s32 *,s32 *); 
+void RTCMenu(void);
